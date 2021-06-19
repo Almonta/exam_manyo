4da80b662da6f23a7314f1b59a341b3c9f567cad
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-19 12:15:39 +0900

    system specに並び替えテストを追加した

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,19 +1,13 @@
 FactoryBot.define do
+
   factory :task do
-    trait :a do
-      id { 1 }
-      task_name { 'manyo' }
-      details { 'manyokadai' }
-    end
-    trait :b do
-      id { 2 }
-      task_name { 'monyo' }
-      details { 'monyokadai' }
-    end
-    trait :c do
-      id { 3 }
-      task_name { 'nonyo' }
-      details { 'nonyokadai' }
-    end
+    task_name { 'manyo' }
+    details { 'manyokadai' }
   end
+  
+  factory :second_task, class: Task do
+    task_name { 'monyo' }
+    details { 'monyokadai' }
+  end
+
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task) { FactoryBot.create(:task) }
+  # let!(:second_task) { FactoryBot.create(:second_task) }
+  before do
+    FactoryBot.create(:task, task_name: 'task_1')
+    FactoryBot.create(:task, task_name: 'task_2')
+    FactoryBot.create(:task, task_name: 'task_3')
+  end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -11,30 +19,38 @@ RSpec.describe 'タスク管理機能', type: :system do
       end
     end
   end
+
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, :a)
+        # task = FactoryBot.create(:task)
+        # FactoryBot.create(:task) #task = がいらないのではと思い試してみた。ダメだった。必要！
         visit tasks_path
         expect(page).to have_content 'manyo'
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        task = FactoryBot.create(:task, :a)
+    #     task = FactoryBot.create(:task)
+    #     task = FactoryBot.create(:second_task)
         visit tasks_path
-        task_list = all('.tasl_row')
-        expect(task_list[0]). to have_content 'nonnyo'        
+        task_list = all('.task_row')
+        binding.irb
+        expect(task_list[0]). to have_content 'manyo'
+        expect(task_list[2]). to have_content 'task_2'
+        expect(task_list[3]). to have_content 'task_3'
       end
     end 
   end
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:task, :a)
+        # task = FactoryBot.create(:task)
+        # FactoryBot.create(:task) #task = がいらないのではと思い試してみた。
         visit task_path(task.id)
         expect(page).to have_content 'manyo'
       end
     end
   end
 end
+
