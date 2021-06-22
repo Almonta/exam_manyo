767bea3385ac7ad9890a4ea3ba566bafe0c84b15
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-22 16:49:36 +0900

    system specをリファクタした

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,12 +3,15 @@ FactoryBot.define do
   factory :task do
     task_name { 'manyo' }
     details { 'manyokadai' }
-    deadline { '2021-06-20'}
+    deadline { '2021-06-22' }
+    status { '着手中' }
   end
   
   factory :second_task, class: Task do
     task_name { 'monyo' }
     details { 'monyokadai' }
+    deadline { '2022-07-23' }
+    status { '未着手' }
   end
 
 end
--- spec/system/task_spec_step3.rb
+++ spec/system/task_spec_step3.rb
@@ -1,14 +1,11 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   let!(:task) { FactoryBot.create(:task) }
-  # let!(:second_task) { FactoryBot.create(:second_task) }
+  let!(:second_task) { FactoryBot.create(:second_task) }
   before do
-    FactoryBot.create(:task, task_name: 'task_1')
-    FactoryBot.create(:task, task_name: 'task_2')
-    FactoryBot.create(:task, task_name: 'task_3')
+    # @task = FactoryBot.create(:task)
+    # FactoryBot.create(:second_task)
   end
-  
-  binding.irb
   
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
@@ -16,9 +13,15 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'task_task_name', with: 'manyo'
         fill_in 'task_details', with: 'manyokadai'
-        fill_in 'task_deadline', with: '2021-06-20'
+        select '2021', from: 'task_deadline_1i'
+        select '6月', from: 'task_deadline_2i'
+        select '22', from: 'task_deadline_3i'
+        select '着手中', from: 'task_status'
         click_on '登録する'
         expect(page).to have_content 'manyo'
+        expect(page).to have_content 'monyo'
+        expect(page).to have_content '2021-06-22'
+        expect(page).to have_content '2022-07-23'
       end
     end
   end
@@ -30,6 +33,9 @@ RSpec.describe 'タスク管理機能', type: :system do
         # FactoryBot.create(:task) #task = がいらないのではと思い試してみた。ダメだった。必要！
         visit tasks_path
         expect(page).to have_content 'manyo'
+        expect(page).to have_content 'manyokadai'
+        expect(page).to have_content 'monyo'
+        expect(page).to have_content 'monyokadai'
       end
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
@@ -38,11 +44,15 @@ RSpec.describe 'タスク管理機能', type: :system do
     #     task = FactoryBot.create(:second_task)
         visit tasks_path
         task_list = all('.task_row')
-        expect(task_list[0]). to have_content 'manyo'
-        expect(task_list[2]). to have_content 'task_2'
-        expect(task_list[3]). to have_content 'task_3'
+        expect(task_list[0]). to have_content 'monyo'
+        expect(task_list[1]). to have_content 'manyo'
       end
-    end 
+    end
+  #   context '終了期限が日付の降順に並んでいる場合' do
+  #     it '終了期限が一番先のタスクが一番上に表示される'
+  #       visit tasks_path
+  #       task_list = all()
+  #   end
   end
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
