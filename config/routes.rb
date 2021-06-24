fa5b172bfcab828824fe92a71555eee42f27593b
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-24 13:55:41 +0900

    ログイン機能を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  get 'sessions/new'
+  # root to: '/'
+  # resources :tasks do
+  #   collection do
+  #     get :/
+  #   end
+  # end
   resources :tasks
   get '/', to: 'tasks#index'
-  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
