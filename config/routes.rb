c79f33a3c614f795b877e7098eeb71a890b0e595
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-24 23:14:02 +0900

    管理画面のCRUD機能を作成した

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   get '/', to: 'tasks#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  namespace :admin do
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
