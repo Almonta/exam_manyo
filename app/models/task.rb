94bcc8a45ccbc2c3999ce3dd1153f1cba4e91dda
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-24 11:41:35 +0900

    sign upページを作成した

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,13 +6,12 @@ class Task < ApplicationRecord
   enum status: { 未着手:1, 着手中:2, 完了:3 }
   enum priority: { 高:1, 中:2, 低:3 }
   
-
   # scope :search_task_name, -> { where("task_name LIKE ?", "%#{params[:search]}%") }
 
   scope :search_task_name, -> (search) { where("task_name LIKE ?", "%#{search}%")}
-
   scope :search_status, -> (search) { where(status: search) }
-
   scope :by_created_at, -> { order(created_at: :desc) }
+
+  belongs_to :user
 
 end
