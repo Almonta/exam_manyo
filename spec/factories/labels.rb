e30a0d04da09c38587b548758b960081401c75a5
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-28 14:54:17 +0900

    タスクの新規作成時にラベルを選択した場合のテストを追加した

--- spec/factories/labels.rb
+++ spec/factories/labels.rb
@@ -2,4 +2,5 @@ FactoryBot.define do
   factory :label do
     name { "test_label" }
   end
+  
 end
--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe 'ラベル機能', type: :system do
   # let!(:task) { FactoryBot.create(:task, user_id: user.id) }
   # let!(:second_task) { FactoryBot.create(:second_task, user_id: user.id) }
   # let!(:third_task) { FactoryBot.create(:third_task, user_id: user.id) }
-  
+  let!(:label) { FactoryBot.create(:label)}
+
   def user_login
     fill_in 'session_email', with: 'test_user1@sample.com'
     fill_in 'session_password', with: 'pass1'
@@ -20,13 +21,15 @@ RSpec.describe 'ラベル機能', type: :system do
   end
   
   before do
-    visit new_session_path
-    user_login
+    # visit new_session_path
+    # user_login
   end
 
   describe 'ラベル登録' do
     context 'ラベルの新規作成をした場合' do
       it 'ラベル一覧に作成したラベルが表示される' do
+        visit new_session_path
+        user_login
         visit tasks_path
         click_on 'ラベル登録'
         sleep 0.5
@@ -36,6 +39,23 @@ RSpec.describe 'ラベル機能', type: :system do
         sleep 0.5
         visit tasks_path
         expect(page).to have_content '万葉'
+      end
+    end
+    context 'タスクの新規作成時にラベルを選択した場合' do
+      it '選択したラベルがタスク一覧画面に表示される' do
+        visit new_session_path
+        user_login
+        visit new_task_path
+        fill_in 'task_task_name', with: 'test_task1'
+        fill_in 'task_details', with: 'test_content1'
+        select '2021', from: 'task[deadline(1i)]'
+        select '6月', from: 'task_deadline_2i'
+        select '22', from: 'task_deadline_3i'
+        select '着手中', from: 'task[status]'
+        check 'test_label'
+        click_on '登録する'
+        # binding.irb
+        expect(page).to have_content 'test_label'
       end
     end
   end
