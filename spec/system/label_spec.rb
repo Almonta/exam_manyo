4621989fcc398ff0236e4e710baa55bce8ec9d47
motohiro takagi <tkg.8.mthr@gmail.com>
2021-06-28 22:20:44 +0900

    検索したラベル名のラベルが付いているタスクのみ表示するテストを追加した

--- spec/system/label_spec.rb
+++ spec/system/label_spec.rb
@@ -41,7 +41,6 @@ RSpec.describe 'ラベル機能', type: :system do
         click_on '登録する'
         sleep 0.5
         visit tasks_path
-        # binding.irb
         expect(page).to have_content '万葉'
       end
     end
@@ -76,16 +75,55 @@ RSpec.describe 'ラベル機能', type: :system do
         check 'test_label1'
         check 'test_label2'
         check 'test_label3'
-        # binding.irb
         click_on '登録する'
+        # binding.irb
         expect(page).to have_content 'test_label1' && 'test_label2' && 'test_label3'
       end
     end
     context 'タスク詳細画面を表示した場合' do
       it 'そのタスクに紐付いているラベルの一覧を表示する' do
+        #ここで選ばれているlabelがなぜtest_label1の1個のみなのかがわからない
+        visit new_session_path
+        user_login
+        visit tasks_path
+        # binding.irb
+        visit task_path(task.id)
+        expect(page).to have_content 'test_label2'
       end
     end
-
+    # let!(:second_task) { FactoryBot.create(:second_task) }
+    # let!(:third_task) { FactoryBot.create(:third_task) }
+    context 'ラベル名で検索した場合' do
+      it '検索したラベル名のラベルが付いているタスクのみ表示する' do
+        # let!(:task) { FactoryBot.create(:task, label:test_label1) }
+        visit new_session_path
+        user_login
+        # label_pull = find(:xpath, "//*[@id="label_id"]")
+        #test_label1のtaskを作成する
+        visit new_task_path
+        fill_in 'task_task_name', with: 'test_task1'
+        fill_in 'task_details', with: 'test_conten1'
+        select '2021', from: 'task[deadline(1i)]'
+        select '6月', from: 'task_deadline_2i'
+        select '22', from: 'task_deadline_3i'
+        select '着手中', from: 'task[status]'
+        check 'test_label1'
+        click_on '登録する'
+        visit tasks_path
+        # select '着手中', from: 'status'
+        select 'test_label2', from: 'label_id'
+        # click_on '検索'
+        # binding.irb
+        find(:xpath, '/html/body/form[2]/div/input').click
+        expect(page).to have_selector '.label_row', text: 'test_label2'
+        expect(page).not_to have_selector '.label_row', text: 'test_label1'
+        #本当はFactoryBotで何種類かのラベルを作ってTaskをいくつか作りたかった。。。。。
+        # sleep
+        # visit tasks_path
+        # select 'test_label1', from: 'label_id'
+        # expect(page).to_not have_content 'text_label2'
+      end
+    end
   end
 end
-
+# binding.irb
