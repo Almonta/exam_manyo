13e289da2677dcf673a90e2e83e1d26e3d0b4e57
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-02 12:37:10 +0900

    マイグレーションファイル修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_020858) do
+ActiveRecord::Schema.define(version: 2021_07_02_033327) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,6 @@ ActiveRecord::Schema.define(version: 2021_06_28_020858) do
 
   create_table "labels", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
